b3861bc236c547a32f272c1dc29e6a9f7795dca2
Joe Naples <jnaples90@gmail.com>
2021-10-14 09:20:08 -0400

    Finished project

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,12 @@
 import React from 'react';
+import styles from './Button.module.css';
 
-import './Button.css';
-
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
+const Button = (props) => {
+    return (
+        <button type={props.type} className={styles.button} onClick={props.onClick}>
+            {props.children}
+        </button>
+    );
 };
 
 export default Button;
